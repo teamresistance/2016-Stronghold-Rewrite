e4e7a1d6ac1aceb3e3bd81269b3da62d3d33d71d
JimHofm52 <JimHofmann52@gmail.com>
2021-10-10 17:08:58 -0400

    Added comments

--- 2016-Stronghold-Rewrite/src/main/java/frc/robot/io/hdw_io/InvertibleSolenoid.java
+++ 2016-Stronghold-Rewrite/src/main/java/frc/robot/io/hdw_io/InvertibleSolenoid.java
@@ -2,15 +2,26 @@ package frc.robot.io.hdw_io;
 
 import edu.wpi.first.wpilibj.Solenoid;
 
-public class InvertibleSolenoid extends Solenoid implements ISolenoid {
+// public class InvertibleSolenoid extends Solenoid implements ISolenoid {
+public class InvertibleSolenoid extends Solenoid {
 
     private final boolean isInverted;
 
-    // Default Solenoid Constructor, not inverted
+    /**
+     * Constructor, default Solenoid Constructor, not inverted
+     * @param module The CAN ID of the PCM the solenoid is attached to.
+     * @param channel The channel on the PCM to control (0..7).
+     */
     public InvertibleSolenoid(int module, int channel) {
         this(module, channel, false);
     }
-    // Constructor to Makes Solenoid Object
+
+    /**
+     * Constructor to Makes Solenoid Object that is invertible.
+     * @param module The CAN ID of the PCM the solenoid is attached to.
+     * @param channel The channel on the PCM to control (0..7).
+     * @param isInverted Invert the command sent to the channel.
+     */
     public InvertibleSolenoid(int module, int channel, boolean isInverted) {
         super(module, channel);
         this.isInverted = isInverted;
