855e58947c6bf8837fa1ed84f842eba98450b64e
JimHofm52 <JimHofmann52@gmail.com>
2021-10-16 19:10:14 -0400

    Added a Framework for a state machine, template.

---
+++ 2016-Stronghold-Rewrite/src/main/java/frc/robot/subsystem/_Framework_SM.java
@@ -0,0 +1,81 @@
+package frc.robot.subsystem;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.io.hdw_io.*;
+import frc.robot.io.joysticks.Button;
+import frc.robot.io.joysticks.JS_IO;
+import frc.util.Timer;
+
+public class _Framework_SM {
+    // Reference or Initialize hardware
+    private static VictorSP snorfMtr = IO.snorfMtr;
+
+    // Reference or Initialize Joystick axis, buttons or pov
+    private static Button btnToggleSnorf = JS_IO.btnToggleSnorf;
+
+    // Create objects for this SM
+    private static int state = 0;
+
+    private static Timer timer = new Timer(0);
+
+    /**
+     * initialize objects for this SM. Ususally called from autonomoousInit or/and
+     * teleopInit
+     */
+    public static void init() {
+
+    }
+
+    /**
+     * Determine the state to select if some outside condition is detected such as a
+     * joystick button press or change of state of a DI.
+     */
+    private static void determ() {
+
+    }
+
+    /**
+     * Periodically Update commands to this subsystem. Normally called from
+     * autonomousPeriodic or teleopPeroic in Robot.
+     */
+    public static void update() {
+        determ();   // Check on external conditions
+        sbdUpdate();// Update Smartdashboard stuff
+
+        switch (state) {
+            case 0: // Off & off. State 0 is normally the default, off.
+                cmdUpdate(false, 0.0);
+                timer.hasExpired(0.0, state);   //Set timer for next state
+                break;
+            case 1: // Normally the kickoff. Ex. drop arm but wait 0.5 seconds to start motor
+                cmdUpdate(true, 0.0);
+                if (timer.hasExpired(0.5, state)) // wait 0.5 seconds to start motor
+                    state++;
+                break;
+            case 2: // Followup state as needed.
+                cmdUpdate(true, 1.0);
+                break;
+            default: // Always have a default, just incase.
+                cmdUpdate(false, 0.0);
+                System.out.println("Bad state for this SMName - " + state);
+        }
+    }
+
+    /**
+     * Update commands for this subsystem. Commands to an object "should" only be
+     * issued from one location.
+     * <p>
+     * Any safeties, things that if not handled will cause damage, should be here.
+     */
+    private static void cmdUpdate(boolean bCmd, double aCmd) {
+
+    }
+
+    /** Update Smartdashbord items */
+    private static void sbdUpdate() {
+        SmartDashboard.putNumber("Snorfler/Motor", snorfMtr.get());
+        SmartDashboard.putBoolean("Snorfler/Btn Toggle", btnToggleSnorf.isDown());
+
+    }
+}
